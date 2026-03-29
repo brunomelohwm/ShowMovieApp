de60b2ed53daa9d1113168d4ca2f7bfdd3b4f977
Bruno Melo <brunomelohwm@gmail.com>
2026-03-29 15:34:31 -0300

    feat:  "refactor: reorganize views, improve bloc flows and add dotenv for API config"

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:show_movie_app/app_widget.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: "env.local");
   await di.init();
   runApp(const MovieApp());
 }
